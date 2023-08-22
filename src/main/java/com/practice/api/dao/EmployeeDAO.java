7169f95eea40f86e5d50e68536fdfbbdcce3d1f0
Bijay Sedhain <bijaysedhain@gmail.com>
2023-08-22 14:14:02 -0500

    Repository

--- src/main/java/com/practice/api/dao/EmployeeDAO.java
+++ src/main/java/com/practice/api/dao/EmployeeDAO.java
@@ -2,6 +2,8 @@ package com.practice.api.dao;
 
 import com.practice.api.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeDAO extends JpaRepository<Employee, Integer> {
 }
