242a291f0dc8c1bbb057d08073422533af282048
Bijay Sedhain <bijaysedhain@gmail.com>
2023-08-22 16:31:24 -0500

    Added Service Layer and its implementation

--- src/main/java/com/practice/api/service/EmployeeService.java
+++ src/main/java/com/practice/api/service/EmployeeService.java
@@ -6,4 +6,13 @@ import java.util.List;
 
 public interface EmployeeService {
     List<Employee> findAll();
+
+    Employee findById(int id);
+
+    Employee addEmployee(Employee employee);
+
+    Employee updateEmployee(int id,Employee employee);
+
+     void deleteById(int id);
+
 }
--- src/main/java/com/practice/api/service/EmployeeServiceImpl.java
+++ src/main/java/com/practice/api/service/EmployeeServiceImpl.java
@@ -2,19 +2,64 @@ package com.practice.api.service;
 
 import com.practice.api.dao.EmployeeDAO;
 import com.practice.api.entity.Employee;
+import com.practice.api.exception.EmployeeNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class EmployeeServiceImpl implements EmployeeService{
+@Slf4j
+public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeDAO employeeDAO;
 
     @Override
     public List<Employee> findAll() {
+        log.info("Fetching all employees");
         return employeeDAO.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Employee addEmployee(Employee employee) {
+        log.info("Adding new employee: {}", employee);
+        return employeeDAO.save(employee);
+    }
+
+    @Override
+    public Employee updateEmployee(int id, Employee employee) {
+        log.info("Updating employee with ID {}: {}", id, employee);
+        Employee existingEmployee = employeeDAO.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found for the ID " + id));
+
+        existingEmployee.setFirstName(employee.getFirstName());
+        existingEmployee.setLastName(employee.getLastName());
+        existingEmployee.setEmail(employee.getEmail());
+
+        return employeeDAO.save(existingEmployee);
+    }
+
+    @Override
+    public Employee findById(int id) {
+        log.info("Fetching employee by ID: {}", id);
+        return employeeDAO.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found for the ID " + id));
+    }
+
+    @Override
+    public void deleteById(int id) {
+        log.info("Deleting employee with ID: {}", id);
+        Optional<Employee> employeeOptional = employeeDAO.findById(id);
+        if (employeeOptional.isPresent()) {
+            employeeDAO.deleteById(id);
+            log.info("Employee with ID {} has been deleted", id);
+        } else {
+            throw new EmployeeNotFoundException("Employee not found for the ID " + id);
+        }
     }
 }
