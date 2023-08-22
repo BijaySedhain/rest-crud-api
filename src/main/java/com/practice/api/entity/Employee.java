9e0acb76acde11fd2657d8ba9c6cff0b3df19c4c
Bijay Sedhain <bijaysedhain@gmail.com>
2023-08-22 14:00:18 -0500

    Created Employee Entity

---
+++ src/main/java/com/practice/api/entity/Employee.java
@@ -0,0 +1,30 @@
+package com.practice.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+@Data
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
